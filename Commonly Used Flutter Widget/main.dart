b8141d43989d45806395cd43984b4399c922c4a0
ASIM BERA <46041809+asimnit@users.noreply.github.com>
2020-07-04 08:05:29 +0530

    Create main.dart

---
+++ Commonly Used Flutter Widget/main.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+import 'package:ticket_book/app_screens/home.dart';
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Exploting UI widgets",
+      home: Home()
+    )
+  );
+}
